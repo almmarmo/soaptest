0e5bb647057b2a9d557477ac86462f497a243554
Andre Marmo <andre.marmo@g.globo>
2021-09-09 21:47:43 -0300

    Add project files.

---
+++ SoapTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SoapTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.MigracaoSisComSoapClient a = new ServiceReference1.MigracaoSisComSoapClient(ServiceReference1.MigracaoSisComSoapClient.EndpointConfiguration.MigracaoSisComSoap, "http://jbhaafisr002.corp.tvglobo.com.br/migracaosiscom/migracaosiscom.asmx");
+            var b = a.ObterInfoProgramasAsync("SP1", "2021090910").Result;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
